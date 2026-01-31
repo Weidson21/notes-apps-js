894dfd9e528d6948c29254ec3092ee4e74dc4eba
Weidson21 <weidson869@gmail.com>
2026-01-30 21:56:01 -0300

    criação do botão

--- notas.js
+++ notas.js
@@ -1,5 +1,8 @@
-// === Local  Storage ===
+// Criar variávael do conteiner e botão
+const tarefas = document.querySelector(".container-notas");
+const bntAdicionar = document.querySelector(".adicionar");
 
+// === Local  Storage ===
 let notas = JSON.parse(localStorage.getItem("notas")) || [];
 
 function salvarLocalStorage(){
