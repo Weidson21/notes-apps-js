4b57cd1d92a1fd3283f7aa6e4055612f8e57d8a7
Weidson21 <weidson869@gmail.com>
2026-02-03 02:27:31 -0300

    Criação da tela de criar notas

--- notas.js
+++ notas.js
@@ -1,6 +1,9 @@
 // Criar variávael do conteiner e botão
-const tarefas = document.querySelector(".container-notas");
+const containerInput = document.querySelector(".container-input")
+const listaNotas = document.querySelector(".container-notas");
+const header = document.querySelector("header");
 const bntAdicionar = document.querySelector(".adicionar");
+
 
 // === Local  Storage ===
 let notas = JSON.parse(localStorage.getItem("notas")) || [];
@@ -8,3 +11,56 @@ let notas = JSON.parse(localStorage.getItem("notas")) || [];
 function salvarLocalStorage(){
     localStorage.setItem("notas", JSON.stringify(notas));
 }
+
+function criarNota(texto, categoria){
+
+    const inputNota = document.querySelector("#titulo");
+    const textarea = document.querySelector("#texto");
+
+    const novaData = {
+        id: Date.now(),
+        titulo: inputNota.value.trim(),
+        texto: textarea.value.trim(),
+        categoria,
+        criadaEm: new Date().toISOString(),
+        atualizadaEm: new Date().toISOString()
+    };
+
+    notas.push(novaNota);
+    salvarLocalStorage();
+    renderizar();
+}
+
+function renderizar(){
+    listaNotas.innerHTML = "";
+
+    notas.forEach(nota => {
+        const div = document.createElement("div");
+        div.classList.add("nota");
+
+        div.innerHTML = `
+        <h3>${nota.titulo}</h3>
+        <p>${nota.texto}</p>
+        <small>${nota.categoria}</small>`;
+
+        listaNotas.appendChild(div);
+    });
+}
+
+bntAdicionar.addEventListener("click", () =>{
+    containerInput.style.display = "none";
+    listaNotas.style.isplay = "none";
+    
+    document.querySelector(".cabeçalho").innerHTML = "Editar Nota";
+
+    const imgVoltar = document.createElement("img");
+    imgVoltar.src = "imagens/seta-para-a-esquerda.png";
+    imgVoltar.classList.add("voltar");
+
+    const span = document.createElement("span");
+    span.textContent = "Voltar";
+    span.style.color = "White";
+    span.classList.add("span");
+
+    header.append(imgVoltar, span);
+});
